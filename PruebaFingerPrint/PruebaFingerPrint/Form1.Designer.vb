f2321ea2750d6496639f21220a56d96a4b5494d4
EduardoCastro15 <67349735+EduardoCastro15@users.noreply.github.com>
2021-10-09 23:04:19 -0500

    Add files via upload

---
+++ PruebaFingerPrint/PruebaFingerPrint/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.imagenHuella = New System.Windows.Forms.PictureBox()
+        Me.vecesDedo = New System.Windows.Forms.Label()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.imagenHuella, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'imagenHuella
+        '
+        Me.imagenHuella.Location = New System.Drawing.Point(25, 12)
+        Me.imagenHuella.Name = "imagenHuella"
+        Me.imagenHuella.Size = New System.Drawing.Size(287, 382)
+        Me.imagenHuella.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.imagenHuella.TabIndex = 0
+        Me.imagenHuella.TabStop = False
+        '
+        'vecesDedo
+        '
+        Me.vecesDedo.AutoSize = True
+        Me.vecesDedo.Location = New System.Drawing.Point(25, 397)
+        Me.vecesDedo.Name = "vecesDedo"
+        Me.vecesDedo.Size = New System.Drawing.Size(130, 15)
+        Me.vecesDedo.TabIndex = 1
+        Me.vecesDedo.Text = "Necesitas pasar el dedo"
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Enabled = False
+        Me.btnGuardar.Location = New System.Drawing.Point(418, 98)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 2
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Enabled = False
+        Me.txtNombre.Location = New System.Drawing.Point(377, 52)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 23)
+        Me.txtNombre.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(334, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Nombre:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(523, 418)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.vecesDedo)
+        Me.Controls.Add(Me.imagenHuella)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.imagenHuella, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents imagenHuella As PictureBox
+    Friend WithEvents vecesDedo As Label
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ PruebaFingerPrint/PruebaFingerPrint/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System.Text
+Imports DPFP
+Imports DPFP.Capture
+
+Public Class Form1
+    Implements DPFP.Capture.EventHandler
+
+    Private captura As DPFP.Capture.Capture
+    Private enroller As DPFP.Processing.Enrollment
+    Private Delegate Sub _delegadoMuestra(ByVal text As String)
+    Private Delegate Sub _delegadoControl()
+    Private template As DPFP.Template
+
+    Private Sub mostrarVeces(ByVal texto As String)
+        If vecesDedo.InvokeRequired Then
+            Dim deleg As New _delegadoMuestra(AddressOf mostrarVeces)
+            Me.Invoke(deleg, New Object() {texto})
+        Else
+            vecesDedo.Text = texto
+        End If
+    End Sub
+
+    Private Sub modificarControles()
+        If (btnGuardar.InvokeRequired) Then
+            Dim deleg As New _delegadoControl(AddressOf modificarControles)
+            Me.Invoke(deleg, New Object() {})
+        Else
+            btnGuardar.Enabled = True
+            txtNombre.Enabled = True
+        End If
+    End Sub
+    Protected Overridable Sub Init()
+        Try
+            captura = New Capture()
+            If Not captura Is Nothing Then
+                captura.EventHandler = Me
+                enroller = New DPFP.Processing.Enrollment()
+                Dim Text As New StringBuilder()
+                Text.AppendFormat("Necesitas pasar el dedo {0} veces", enroller.FeaturesNeeded)
+                vecesDedo.Text = Text.ToString()
+            Else
+                MessageBox.Show("No se pudo instanciar la captura")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("No se pudo inicializar la captura")
+        End Try
+    End Sub
+
+    Protected Sub IniciarCaptura()
+        If Not captura Is Nothing Then
+            Try
+                captura.StartCapture()
+            Catch ex As Exception
+                MessageBox.Show("No se pudo iniciar la captura")
+            End Try
+        End If
+
+    End Sub
+
+    Protected Sub PararCaptura()
+        If Not captura Is Nothing Then
+            Try
+                captura.StopCapture()
+            Catch ex As Exception
+                MessageBox.Show("No se pudo detener la captura")
+            End Try
+        End If
+    End Sub
+
+    Public Sub OnComplete(Capture As Object, ReaderSerialNumber As String, Sample As Sample) Implements EventHandler.OnComplete
+        ponerImagen(ConvertirSampleAMapaDeBits(Sample))
+        Procesar(Sample)
+    End Sub
+
+    Public Sub OnFingerGone(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnFingerGone
+
+    End Sub
+
+    Public Sub OnFingerTouch(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnFingerTouch
+        'MessageBox.Show("Se ha tocado el aparato")
+    End Sub
+
+    Public Sub OnReaderConnect(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnReaderConnect
+
+    End Sub
+
+    Public Sub OnReaderDisconnect(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnReaderDisconnect
+
+    End Sub
+
+    Public Sub OnSampleQuality(Capture As Object, ReaderSerialNumber As String, CaptureFeedback As CaptureFeedback) Implements EventHandler.OnSampleQuality
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init()
+        IniciarCaptura()
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        PararCaptura()
+    End Sub
+
+    Protected Function ConvertirSampleAMapaDeBits(ByVal sample As DPFP.Sample) As Bitmap
+        Dim convertidor As New DPFP.Capture.SampleConversion() 'variable tipo conversor de DPFP.Sample
+        Dim mapaBits As Bitmap = Nothing
+        convertidor.ConvertToPicture(sample, mapaBits)
+        Return mapaBits
+    End Function
+
+    Private Sub ponerImagen(ByVal bmp)
+        imagenHuella.Image = bmp
+    End Sub
+
+    Protected Function extrarCaracteristicas(ByVal Sample As DPFP.Sample, ByVal Purpose As DPFP.Processing.DataPurpose) As DPFP.FeatureSet
+        Dim extractor As New DPFP.Processing.FeatureExtraction()
+        Dim alimentacion As New DPFP.Capture.CaptureFeedback
+        alimentacion = DPFP.Capture.CaptureFeedback.None
+        Dim caracteristicas As New DPFP.FeatureSet()
+        extractor.CreateFeatureSet(Sample, Purpose, alimentacion, caracteristicas)
+        If (alimentacion = DPFP.Capture.CaptureFeedback.Good) Then
+            Return caracteristicas
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Protected Sub Procesar(ByVal Sample As DPFP.Sample)
+        Dim caracteristicas As DPFP.FeatureSet = extrarCaracteristicas(Sample, DPFP.Processing.DataPurpose.Enrollment)
+        If (Not caracteristicas Is Nothing) Then
+            Try
+                enroller.AddFeatures(caracteristicas)
+            Finally
+                Dim Text As New StringBuilder()
+                Text.AppendFormat("Necesitas pasar el dedo {0} veces", enroller.FeaturesNeeded)
+                mostrarVeces(Text.ToString())
+                Select Case enroller.TemplateStatus
+                    Case DPFP.Processing.Enrollment.Status.Ready
+                        template = enroller.Template
+                        PararCaptura()
+                        modificarControles()
+                    Case DPFP.Processing.Enrollment.Status.Failed
+                        enroller.Clear()
+                        PararCaptura()
+                        IniciarCaptura()
+                End Select
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Dim imagen As Bitmap
+        Dim guardar As SaveFileDialog = New SaveFileDialog()
+        guardar.Filter = "Bitmap|*.bmp|PNG|*.png"
+        If guardar.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            imagen = imagenHuella.Image
+            imagen.Save(guardar.FileName)
+        End If
+    End Sub
+End Class
