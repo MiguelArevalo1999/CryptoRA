33afaf62e32b8d9ae4e87230f20b02a3d30c9dfe
EduardoCastro15 <georgecastrocruces1515@gmail.com>
2021-10-29 17:55:56 -0500

    Modificación a todas la ventanas

---
+++ CryptoRA/CryptoRA/FormNuevoUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoRA
+{
+    public partial class FormNuevoUsuario : Form
+    {
+        public FormNuevoUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegresar_Click(object sender, EventArgs e)
+        {
+            Form formulario1 = new FormInicio();
+            formulario1.Show();
+            this.Hide();
+        }
+    }
+}
