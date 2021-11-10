26ac55ad274ad31adb6f25c8af9b20f98f2d861d
Miguel Arévalo <miguelarevalo1999@gmail.com>
2021-11-10 15:09:49 -0600

    Data set mejorado y guardado en Data set enhanced usando filtros de Gabor  listo

--- LecturaYGuardadoHuellas/LecturaYGuardadoHuellas/Data set/gaborfilter.py
+++ LecturaYGuardadoHuellas/LecturaYGuardadoHuellas/Data set/gaborfilter.py
@@ -1,20 +1,22 @@
 import sys
-from src.FingerprintImageEnhancer import FingerprintImageEnhancer
+from FingerprintImageEnhancer import FingerprintImageEnhancer
 import cv2
 
 if __name__ == '__main__':
 
-    image_enhancer = FingerprintImageEnhancer()         # Create object called image_enhancer
-    if(len(sys.argv)<2):                                # load input image
-        print('loading sample image');
-        img_name = '2.jpg'
-        img = cv2.imread('../images/' + img_name)
-    elif(len(sys.argv) >= 2):
-        img_name = sys.argv[1];
-        img = cv2.imread('../images/' + img_name)
+    for iteracion in range(1,11):
+        image_enhancer = FingerprintImageEnhancer()         # Create object called image_enhancer
+        if(len(sys.argv)<2):                                # load input image
+            print('loading sample image')
+            img_name = 'h'+ str(iteracion)+'.bmp'
+            img = cv2.imread("LecturaYGuardadoHuellas\\LecturaYGuardadoHuellas\\Data set\\Yvonne\\"+img_name)
+            
+        elif(len(sys.argv) >= 2):
+            img_name = sys.argv[1]
+            img = cv2.imread('../images/' + img_name)
 
-    if(len(img.shape)>2):                               # convert image into gray if necessary
-         img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        if(len(img.shape)>2):                               # convert image into gray if necessary
+            img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    out = image_enhancer.enhance(img)     # run image enhancer
-    image_enhancer.save_enhanced_image('../enhanced/' + img_name)   # save output
+        out = image_enhancer.enhance(img)     # run image enhancer
+        image_enhancer.save_enhanced_image("LecturaYGuardadoHuellas\\LecturaYGuardadoHuellas\\Data set enhanced\\Yvonne\\" + img_name)   # save output
